4093b4fa553028768eb06de9a6cd89803338bc10
fahri-r <00.fahri.r@gmail.com>
2023-11-24 13:41:17 +0700

    add notion section

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,13 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { customTheme } from "@/lib/customTheme";
 import Fonts from "@/components/Fonts";
 
+import 'react-notion-x/src/styles.css'
+import 'prismjs/themes/prism-tomorrow.css'
+import 'katex/dist/katex.min.css'
+import 'katex/dist/katex.min.css'
+
+import '@/styles/notion.css'
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={customTheme}>
---
+++ pages/the-untold-story/[slug].tsx
@@ -0,0 +1,81 @@
+import { Inter } from "next/font/google";
+import React from "react";
+import SectionLayout from "@/components/layouts/SectionLayout";
+import { Heading } from "@chakra-ui/react";
+import { motion } from "framer-motion";
+import { PortfolioItemProps } from "@/data/portfolio";
+import ProjectLayout from "@/components/layouts/ProjectLayout";
+import { NotionAPI } from "notion-client";
+import { NotionRenderer } from "react-notion-x";
+import { gradient } from "@/lib/gradient";
+
+const inter = Inter({ subsets: ["latin"] });
+interface Props {
+  data: PortfolioItemProps;
+}
+
+export default function Home({ data }: Props) {
+  const initialBox = {
+    rest: {
+      scale: 1,
+      transition: {
+        duration: 0.3,
+        ease: "easeInOut",
+      },
+    },
+    initial: {
+      scale: [0, 1],
+      transition: {
+        duration: 0.3,
+        ease: "easeInOut",
+      },
+    },
+    hover: {
+      scale: 1,
+      transition: {
+        duration: 0,
+        ease: "easeInOut",
+      },
+    },
+  };
+  const hoverBox = {
+    rest: {
+      scale: [1.1, 1],
+      transition: {
+        duration: 0.2,
+        ease: "easeInOut",
+      },
+    },
+    hover: {
+      scale: [1, 1.1],
+      filter: "brightness(50%)",
+      transition: {
+        duration: 0.2,
+        ease: "easeInOut",
+      },
+    },
+  };
+  return (
+    <ProjectLayout github={""} site={""} title={""} thumbnail={""}>
+      <SectionLayout title={"The Untold Story"}>
+        <NotionRenderer
+          recordMap={data}
+          fullPage={true}
+          darkMode={true}
+          disableHeader
+        />
+      </SectionLayout>
+    </ProjectLayout>
+  );
+}
+
+export async function getServerSideProps(context: any) {
+  const slug = context.query.slug;
+  const notion = new NotionAPI();
+  const data = await notion.getPage(slug);
+  return {
+    props: {
+      data,
+    },
+  };
+}
